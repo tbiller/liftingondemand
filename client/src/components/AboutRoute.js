a859e6b2f334e1f66a1c84df5c2949326654da45
Trevor <tbiller@gmail.com>
2017-11-18 22:44:16 -0500

    changed about page

--- client/src/components/AboutRoute.js
+++ client/src/components/AboutRoute.js
@@ -13,14 +13,14 @@ class AboutRoute extends Component {
 				<div className='title'>About</div>
 				<div className='about-content'>
 					<p>
-						Lifting On Demand is the home for powerlifting competition videos.
+						Lifting On Demand tries to make powerlifting competitions easier to watch and rewatch.
 					</p>
 					<p>
 						We use a computer script to help us detect when lifters are on competition livestreams. That saves us from recording all
 						the timestamps by hand. Computer identification does mean that we will get it wrong from time to time.
 					</p>
 					<p>
-						Something wrong with a timestamp or video? Contact us at 
+						Something wrong with a timestamp or video? Contact us at
 						<span className='email-address'>support@liftingondemand.com</span>to report any errors, or if you have any other questions or ideas for the site.
 					</p>
 					<p>On a mobile device? Mobile browsers can be finicky, visit the site on desktop if things aren't working right.</p>
